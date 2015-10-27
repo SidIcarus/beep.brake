cfdb09dc236aebcdb82e752f914359958ab377a6
Kevin Mulligan <kam9115@rit.edu>
2015-10-27 15:42:17 -0400

    More skeleton files

---
+++ app/src/main/c++/buffer/DiskWriter.h
@@ -0,0 +1,12 @@
+/*
+ * Header file for DiskWriter object
+ *
+ * Responsible for writing segments to disk
+ */
+
+#ifndef __BUFFER_MANAGEMENT__DISK_WRITER_H_
+#define __BUFFER_MANAGEMENT__DISK_WRITER_H_
+
+
+
+#endif /*__BUFFER_MANAGEMENT__DISK_WRITER_H_*/
---
+++ app/src/main/c++/buffer/SegmentBuffer.h
@@ -0,0 +1,13 @@
+/*
+ * Header file for SegmentBuffer object
+ *
+ * Maintains a buffer of segments of at least the minimum required time
+ * 		(i.e. 6 seconds currently)
+ */
+
+#ifndef __BUFFER_MANAGEMENT__SEGMENT_BUFFER_H_
+#define __BUFFER_MANAGEMENT__SEGMENT_BUFFER_H_
+
+
+
+#endif /*__BUFFER_MANAGEMENT__SEGMENT_BUFFER_H_*/
