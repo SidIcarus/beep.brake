20376c34e3b0c3f2be7e96d8bcf0f7c50063eb2a
Alvaro Pareja-Lecaros <Alvaro Pareja-Lecaros>
2015-11-12 15:52:42 -0500

    Updated gitignore

--- opencv_java3/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ opencv_java3/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\app61\\Git\\Github\\sp\\beep.brake\\opencv_java3\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\app61\\Git\\Github\\beep.brake\\opencv_java3\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
