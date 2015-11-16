e3c2b1ff344c3d2f260256344ba6d3f33a4a3c45
Alvaro Pareja-Lecaros <Alvaro Pareja-Lecaros>
2015-11-16 01:11:35 -0500

    Added native folder to repo. Android.mk now references packaged native folder. ndkBuild in app/build.gradle is OS conscious

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,16 +1,22 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH  := $(call my-dir)
+JNI_ROOT    := $(LOCAL_PATH)/../
+MAIN_ROOT   := $(JNI_ROOT)/../
+SRC_ROOT    := $(MAIN_ROOT)/../
+APP_ROOT    := $(SRC_ROOT)/../
+PROJECT_ROOT:= $(APP_ROOT)/../
 
 include $(CLEAR_VARS)
 
 #opencv
-OPENCVROOT:= C:/Users/app61/AppData/Local/Android/OpenCV-3.0.0-android-sdk
+OPENCV_ROOT:= $(APP_ROOT)/opencv_java3
 OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
-include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
+include ${OPENCV_ROOT}/native/jni/OpenCV.mk
 
-LOCAL_SRC_FILES := edu_rit_margikarpets_beepbrake_MainActivity.c
 LOCAL_LDLIBS += -llog
 LOCAL_MODULE := splash
+LOCAL_SRC_FILES := edu_rit_margikarpets_beepbrake_MainActivity.c
+#LOCAL_SRC_FILES += new_src_file.c #add more source files like this
 
 include $(BUILD_SHARED_LIBRARY)
