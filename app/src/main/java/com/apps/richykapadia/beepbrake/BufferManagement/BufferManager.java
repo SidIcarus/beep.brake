5f36bb7aea74031c1b526c31ba8f035c4cddbca9
KevinMulligan <kamskywalker@gmail.com>
2016-01-14 13:17:25 -0500

    Added stubs, some with content

--- app/src/main/java/com/apps/richykapadia/beepbrake/BufferManagement/BufferManager.java
+++ app/src/main/java/com/apps/richykapadia/beepbrake/BufferManagement/BufferManager.java
@@ -1,5 +1,22 @@
 package com.apps.richykapadia.beepbrake.BufferManagement;
 
+import myfirstapp.app.Segment;
+
 public class BufferManager {
+    private SegmentBuffer buffer;
+
+    public BufferManager() {
+        buffer = new SegmentBuffer();
+    }
+
+    public Segment getNewestSetment() {
+
+        return null;
+    }
+
+    public void addSegment() {
+
+    }
+
 
 }
--- app/src/main/java/com/apps/richykapadia/beepbrake/BufferManagement/SegmentBuffer.java
+++ app/src/main/java/com/apps/richykapadia/beepbrake/BufferManagement/SegmentBuffer.java
@@ -1,13 +1,60 @@
 package com.apps.richykapadia.beepbrake.BufferManagement;
 
-import java.util.LinkedList;
-import java.util.List;
+
+import myfirstapp.app.Segment;
 
 public class SegmentBuffer {
-    private List buffer;
+    private Segment newest;
+    private Segment oldest;
 
+    private final int timediff = 6000;//max milliseconds to keep old segments
+
+    /**
+     * Constructor
+     * Start with empty newest and oldest segments
+     */
     public SegmentBuffer() {
-        //buffer = new LinkedList<Segment>();
+        this.newest = null;
+        this.oldest = null;
+    }
+
+    /**
+     * Add this segment to the buffer
+     * Trims old segments after adding
+     * @param seg - The segment to add
+     */
+    public void addSegment(Segment seg) {
+        if(newest == null) {
+            newest = seg;
+            oldest = seg;
+        } else {
+            newest.setNextSeg(seg);
+            newest = seg;
+            prune();
+        }
+    }
+
+    /**
+     * Get the latest segment from the buffer
+     * @return newest - the latest segment
+     */
+    public Segment getNewest() {
+        return newest;
+    }
+
+    /**
+     * Save the current buffer to file using the given diskwriter
+     * @param dw - diskwriter to send the contents to
+     */
+    public void save(DiskWriter dw) {
+
+    }
+
+    /**
+     * Go through the segments starting from the oldest and remove any that are not needed anymore
+     */
+    private void prune() {
+
     }
 
 }
