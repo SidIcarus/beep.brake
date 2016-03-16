096408cfdb4a0c2a1bbb87b54679cb9c839c986f
Kevin Mulligan <kam9115@rit.edu>
2016-03-16 14:49:04 -0400

    Switched to saving images directly to zip file. Still need to add JSON file, and test.

--- app/src/main/java/edu/rit/se/beepbrake/buffer/DiskWriter.java
+++ app/src/main/java/edu/rit/se/beepbrake/buffer/DiskWriter.java
@@ -8,15 +8,19 @@ import android.util.Log;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfInt;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import edu.rit.se.beepbrake.Segment;
 
@@ -59,19 +63,25 @@ public class DiskWriter extends Thread implements Runnable{
      * Dereferences as it goes to cut down on memory usage
      */
     public void run() {
-        //File name = 'androidID'_'eventID'
+        //File name format: 'androidID'_'eventID'
         String deviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-        String fileName = deviceId + "_" + String.valueOf(eventId) + ".json";
+        String baseName = deviceId + "_" + String.valueOf(eventId);
+        String fileName = baseName + ".json";
         FileOutputStream fos;
+        ZipOutputStream zos;
 
         try {
             //fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
             File writeDir = new File(path);
             if(!writeDir.exists()) {
+                //noinspection ResultOfMethodCallIgnored
                 writeDir.mkdirs();
             }
             File event = new File(path, fileName);
             fos = new FileOutputStream(event);
+
+            //open the Zip File
+            zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(baseName + ".zip")));
 
             //Print file header
             fos.write(String.valueOf("{\"deviceid\":\"" + deviceId + "\",").getBytes());
@@ -102,21 +112,23 @@ public class DiskWriter extends Thread implements Runnable{
                     } else {
                         fos.write(String.valueOf(',').getBytes());
                     }
-                    writeSegment(s, fos);
+                    writeSegment(s, fos, zos);
                 }
             }
             //We've been notified that all segments are in the queue, but we may not have
             // written all of them to disk yet
             while((s = segments.poll()) != null) {
                 fos.write(String.valueOf(',').getBytes());
-                writeSegment(s, fos);
+                writeSegment(s, fos, zos);
             }
 
             //Close of segments and json
             fos.write(String.valueOf("]}").getBytes());
 
             fos.close();
+            zos.close();
 
+            /*
             //Now zip the file
             Log.e("buferSystem", "Started zipping");
             File[] files = writeDir.listFiles();
@@ -129,7 +141,7 @@ public class DiskWriter extends Thread implements Runnable{
             ZipFiles zf = new ZipFiles(names.toArray(str), fileName);
             zf.zip();
             Log.e("buferSystem", "Finished zipping");
-
+            */
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -165,7 +177,7 @@ public class DiskWriter extends Thread implements Runnable{
      * @param seg - the segment to read
      * @param file - the file to write to
      */
-    private void writeSegment(Segment seg, FileOutputStream file) throws IOException{
+    private void writeSegment(Segment seg, FileOutputStream file, ZipOutputStream zip) throws IOException{
         //Segment header
         file.write(String.valueOf("{\"segtime\":" + String.valueOf(seg.getCreatedAt())).getBytes());
         file.write(String.valueOf(",\"sensordata\": [").getBytes());
@@ -175,7 +187,15 @@ public class DiskWriter extends Thread implements Runnable{
             String imgName = String.valueOf(seg.getCreatedAt()) + ".png";
             String filepath = path + imgName;
             MatOfInt param = new MatOfInt(Imgcodecs.CV_IMWRITE_PNG_COMPRESSION);
-            Imgcodecs.imwrite(filepath, seg.getImg(), param);
+            //Imgcodecs.imwrite(filepath, seg.getImg(), param);
+
+            MatOfByte buf = new MatOfByte();
+            Imgcodecs.imencode(filepath, seg.getImg(), buf, param);
+
+            ZipEntry ze = new ZipEntry(imgName);
+            zip.putNextEntry(ze);
+            zip.write(buf.toArray());
+
             file.write(String.valueOf("{\"key\":\"imagename\",\"value\":\"" + filepath + "\"}").getBytes());
         }
         for(String k : seg.getKeys()) {
